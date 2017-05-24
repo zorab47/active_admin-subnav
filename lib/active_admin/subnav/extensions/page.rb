3a0bec0db3fc1ad928ea6f1d6f85e816bf2c898f
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2017-05-24 12:05:53 -0500

    Add custom belongs_to override to Pages

--- lib/active_admin/subnav/extensions/page.rb
+++ lib/active_admin/subnav/extensions/page.rb
@@ -1,11 +1,13 @@
 module ActiveAdmin
-  module Subnav
-    module Page
-      extend ActiveSupport::Concern
+  class Page
+    def belongs_to(target, options = {})
+      @belongs_to = Resource::BelongsTo.new(self, target, options)
+      self.sub_navigation_menu_name = target
+      controller.send :belongs_to, target, options.dup
+    end
 
-      def show_sub_menu?(*)
-        false
-      end
+    def show_sub_menu?(*)
+      false
     end
   end
 end
--- lib/active_admin/subnav/extensions/resource.rb
+++ lib/active_admin/subnav/extensions/resource.rb
@@ -1,7 +1,5 @@
 module ActiveAdmin
   class Resource
-
-    # TODO: Support for optional belongs_to
     def belongs_to(target, options = {})
       @belongs_to = Resource::BelongsTo.new(self, target, options)
       self.sub_navigation_menu_name = target
